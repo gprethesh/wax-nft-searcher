b4caf9b5e8ff8f64c86044a1935622be49291706
George <georgeprethesh@georges-MacBook-Air.local>
2022-10-27 18:11:16 +0530

    Few changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let collectionName = []
 let userName = []
 
 
-/* Query Selectors */
+/* QUERY SELECTORS */
 const mainx = document.querySelector(".mainx")
 const wholeFrameDiv = document.querySelector(".whole-frame-div")
 const frameDivError = document.querySelector(".frame-div")
@@ -16,16 +16,19 @@ const pageinfox = document.querySelector(".pageinfo")
 /* URLS */
 const srcOfImg = "https://ipfs.wecan.dev/ipfs/"
 const urlatomic = "https://wax.api.atomicassets.io"
+const srcOfvid = "https://atomichub-ipfs.com/ipfs/"
 
 // MAIN FUNCTION FOR FETTCHING NFT
 const runfun = () => {
 
     pagenum.innerHTML = "";
     mainx.innerHTML = "";
-    // Make a request for a user with a given ID
+
+    // MAKE A REQUEST FOR A USER WITH A GIVEN ID
     axios.get(`${urlatomic}/atomicassets/v1/assets?collection_name=${collectionName}&schema_name=&template_id=&owner=${userName}&page=1&limit=100&order=desc&sort=asset_id`)
         .then(function (response) {
-
+            
+            // CLEARING EVERYTHING IN MAINX DIV
             mainx.innerHTML = "";
 
             if (!response.data.data || response.data.data.length === 0) {
@@ -55,12 +58,12 @@ const runfun = () => {
 
                 frameDivError.appendChild(alertDiv) //
 
-                // stropping the code
+                // STROPPING THE CODE IF ERROR
                 throw emptyDataError;
             }
 
 
-            // handle success
+            // HANDLE SUCCESS IF NO ERROR FOUND
             let allnfts = response.data.data
 
             let frameDivX = document.querySelector(".frame-divx")
@@ -68,12 +71,13 @@ const runfun = () => {
             frameDivX.innerHTML += `    <div class="most-popular-div">
             <span class="pop1">Most</span><span class="popular-span"> Popular</span>`
 
+            // FUNCTION TO GET TOTAL NUMBER OF PAGES
             function numPages(cardsArray) {
-                // returns the number of pages
+                // RETURNS THE NUMBER OF PAGES
                 return Math.ceil(cardsArray.length / itemsPerPage)
             }
 
-            // reference to total pages
+            // REFERENCE TO TOTAL PAGES
             const pages = numPages(allnfts)
 
             const pageinfo = document.querySelector(".frame-div")
@@ -99,17 +103,17 @@ const runfun = () => {
 
             function changePage(page) {
 
-                // make sure page is in bounds 
+                // MAKE SURE PAGE IS IN BOUNDS 
                 if (page < 1) page = 1
                 if (page > pages) page = pages
 
-                // clear output containing element
+                // CLEAR OUTPUT CONTAINING ELEMENT
                 mainx.innerHTML = ""
 
 
-                /* TODO: PAGE NUMBER */
+                /* PAGE NUMBER */
 
-                // forloop if current page is lessthan or equal to then add 1+
+                // FORLOOP IF CURRENT PAGE IS LESSTHAN OR EQUAL TO THEN ADD 1+
                 for (let i = 1; i <= currentPage; i++) {
                     pagenumberx.push(i);
                 }
@@ -126,16 +130,18 @@ const runfun = () => {
 
                 pagenum.appendChild(frameDivO) ////
 
-                /* TODO: END HERE */
+                /* END HERE */
 
 
 
-                // for each item within the range of the current page
+                // FOR EACH ITEM WITHIN THE RANGE OF THE CURRENT PAGE
                 for (let i = (page - 1) * itemsPerPage; i < (page * itemsPerPage) && i < allnfts.length; i++) {
 
-                    // append the html to the element
-                    /*        HTML CODE FROM HERE        */
                     let n = allnfts[i]
+
+                    // ADDING ISEXIST PROPERTY FOR API OBJECT
+                    let isExist = "video" in n.data;
+
 
                     // /* MAIN APPEND CODE START */
                     let nftFameDiv = document.createElement("div")
@@ -145,12 +151,27 @@ const runfun = () => {
                     imgNftDiv.classList.add("image-nft-div")
                     nftFameDiv.appendChild(imgNftDiv) //
 
-                    let nftImg = document.createElement("img")
-                    nftImg.classList.add("anonymous-mode-nft-1")
-                    // nftImg.width = "300"
-                    // nftImg.height = "370"
-                    nftImg.src = srcOfImg + n.data.img
-                    imgNftDiv.appendChild(nftImg) //
+                    // CHECKING IF NFT IS A IMAGE OR VIDEO
+                    if (isExist) {
+
+                        let video = document.createElement("video")
+                        video.width = "200"
+                        video.height = "260"
+                        video.setAttribute("controls", "controls")
+                        let csource = document.createElement("source")
+                        csource.src = srcOfvid + n.data.video
+                        csource.setAttribute("type", "video/mp4")
+                        video.appendChild(csource) //
+                        imgNftDiv.appendChild(video) //
+                    } else {
+
+                        let nftImg = document.createElement("img")
+                        nftImg.classList.add("anonymous-mode-nft-1")
+                        nftImg.width = "200"
+                        nftImg.height = "260"
+                        nftImg.src = srcOfImg + n.data.img
+                        imgNftDiv.appendChild(nftImg) //
+                    }
 
                     let textDiv = document.createElement("div")
                     textDiv.classList.add("text-div")
@@ -176,10 +197,10 @@ const runfun = () => {
             }
 
 
-            // running chage Page function
+            // RUNNING CHAGE PAGE FUNCTION
             changePage(1)
 
-            // running Pagination html using this function
+            // RUNNING PAGINATION HTML USING THIS FUNCTION
             paginationfun()
 
             /// PAGINATION FUNCTION NEXT
@@ -208,7 +229,7 @@ const runfun = () => {
         })
 
         .catch(function (error) {
-            // handle error
+            // HANDLE ERROR
         })
 
 }
@@ -227,30 +248,54 @@ let validate = () => {
 }
 
 
-// BUTTON WITH INPUT AND SPLICE INTO ARRAY
+// QUERYSELECTORS
 let buttonz = document.querySelector("#search")
 let walletid = document.querySelector("#walletid")
 let collectionid = document.querySelector("#collection")
 
 
+// BUTTON WITH INPUT AND SPLICE INTO ARRAY
 const pushUsername = () => {
-    validate()
-    let loadafun = document.querySelector(".loadingfun")
-    loadafun.innerHTML += `<div class="spinner-border text-light" role="status">
+    
+    buttonz.innerHTML = "";
+    buttonz.innerHTML += `<div class="spinner-border text-light" role="status">
     <span class="visually-hidden">Loading...</span></div>`
     wholeFrameDiv.innerText = "";
     pagenum.innerHTML = "";
     userName.splice(0, 1, walletid.value);
-    // walletid.value = ""
+    // walletid.value = "" // IF YOU WANT TO REMOVE INPUT VALUE AFTER EXECUTING
     collectionName.splice(0, 1, collectionid.value);
-    // collectionid.value = ""
     runfun()
+    buttonz.setAttribute("disabled", "true")
 }
 
-// Event Listener to run function on click
+// FUNCTION TO CHECK AND HANDLE STATES IF INPUT IS BLANK
+const stateHandle = () => {
+    if (document.querySelector("#walletid").value === "" || document.querySelector("#collection").value === "") {
+        buttonz.setAttribute("disabled", "true") //button remains disabled
+        buttonz.classList.remove("frame-button", "search-div")
+        buttonz.classList.add("frame-buttonx", "search-div")
+    } else {
+        buttonz.removeAttribute("disabled", "true")
+        buttonz.classList.remove("frame-buttonx", "search-div")
+        buttonz.classList.add("frame-button", "search-div")
+    }
+};
+
+walletid.addEventListener("change", stateHandle);
+collectionid.addEventListener("change", stateHandle);
+buttonz.addEventListener("pointerover",stateHandle)
+
+
+stateHandle()
+
+
+
+// EVENT LISTENER TO RUN FUNCTION ON CLICK
 buttonz.addEventListener("click", pushUsername)
 
 
+// FUNCTION FOR SCROLLING UP
 const scrollhere = () => {
     const element = document.getElementById("scrollhere");
     element.scrollIntoView();
@@ -282,12 +327,3 @@ let paginationfun = () => {
     /* CODE END */
 
 }
-
-
-
-
-
-
-
-
-
